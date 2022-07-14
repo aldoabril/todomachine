1d56892d0a4f9b208707da0a0096f54c43c95a0e
Aldo Abril <aldoabril@gmail.com>
2022-07-13 19:35:18 -0500

    chore: version useReducer

--- todomachine/src/Hooks/useLocalStorage.js
+++ todomachine/src/Hooks/useLocalStorage.js
@@ -1,10 +1,21 @@
 import React, { useEffect } from "react";
 
 export const useLocalStorage = (itemName, initialValue) => {
-  const [items, setItems] = React.useState(initialValue);
-  const [loading, setLoading] = React.useState(true);
-  const [error, setError] = React.useState("");
-  const [localStorageChanged, setLocalStorageChanged] = React.useState(false);
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+  const {items,
+    loading,
+    error,
+    localStorageChanged} = state;
+  
+  const onError = (error)=>{
+    dispatch({type: actionTypes.error, payload: error})
+  }
+  const onSuccess = (items)=>{
+    dispatch({type: actionTypes.success, payload: items})
+  }
+  const onSave = (items)=>{
+    dispatch({type: actionTypes.save, payload: items})
+  }
   useEffect(() => {
     setTimeout(() => {
       try {
@@ -15,11 +26,10 @@ export const useLocalStorage = (itemName, initialValue) => {
         } else {
           parseItems = JSON.parse(localStorageItems);
         }
-        setItems(parseItems);
-        setLoading(false);
-        setLocalStorageChanged(false);
+        onSuccess(parseItems)
+        
       } catch (err) {
-        setError(error);
+        onError(err)
       }
     }, 2000);
   }, [localStorageChanged]);
@@ -28,15 +38,75 @@ export const useLocalStorage = (itemName, initialValue) => {
     try {
       const stringifyItems = JSON.stringify(items);
       localStorage.setItem(itemName, stringifyItems);
-      setItems(items);
+      onSave(items);
     } catch (err) {
-      setError(err);
+      onError(err);
     }
   };
 
   const reload = ()=>{
-    setLocalStorageChanged(true)
-    setLoading(true)
+    dispatch({type: actionTypes.reload})
+    
   }
   return { items, saveItems, loading, error, reload, localStorageChanged };
 };
+
+
+const actionTypes = {
+  success : 'SUCCESS',
+  save: 'SAVE',
+  loading: 'LOADING',
+  error: 'ERROR',
+  reload: 'RELOAD',
+  localStorageChanged: 'LOCAL_STORAGE_CHANGED'
+}
+
+const initialState = {
+  items:[],
+  loading:true,
+  error:'',
+  localStorageChanged:false
+}
+
+const reducer = (state, action)=>{
+  switch (action.type){
+    case actionTypes.error:
+      return {
+            ...state,
+            error: action.payload
+          }
+     case actionTypes.loading:
+      return {
+        ...state,
+        loading: true,
+      }
+     case actionTypes.success:
+      return {
+        ...state,
+        items: action.payload,
+        loading: false,
+        localStorageChanged: false
+      }
+      case actionTypes.save:
+        return {
+          ...state,
+          items: action.payload,
+          loading: false,
+          localStorageChanged: false
+        }
+
+     case actionTypes.reload:
+       return {
+        ...state,
+        localStorageChanged: true,
+        loading: true,
+       }   
+     case actionTypes.localStorageChanged:
+      return {
+        ...state,
+        localStorageChanged: true
+      }
+      default:
+        return state      
+  }
+}
